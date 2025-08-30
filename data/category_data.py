3808e433576136b32520bb55bf2654178919d32d
sara-jaffar <sara.alshoula55@email.com>
2025-08-30 11:06:21 +0300

    created the category_data file and updated the seed file

---
+++ data/category_data.py
@@ -0,0 +1,10 @@
+from models.category import CategoryModel
+
+categories_list = [
+    CategoryModel(name="Yoga", description="Relaxing and flexibility-focused classes."),
+    CategoryModel(name="Pilates", description="Core-strengthening and posture-improving sessions."),
+    CategoryModel(name="Cardio", description="High-energy classes to improve endurance and heart health."),
+    CategoryModel(name="Strength Training", description="Weight-based sessions to build muscle and strength."),
+    CategoryModel(name="HIIT", description="High-Intensity Interval Training for fat burning and stamina."),
+    CategoryModel(name="Zumba", description="Fun dance-based workouts to keep you moving."),
+]
