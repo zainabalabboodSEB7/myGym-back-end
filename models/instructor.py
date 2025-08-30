aa139ab1df619b43c59d19410fc6088baf38aef8
Dana <danalkhashram@gmail.com>
2025-08-30 10:42:23 +0300

    update instructor model and serializer

--- models/instructor.py
+++ models/instructor.py
@@ -1,14 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 from sqlalchemy.orm import relationship
-from database import BaseModel
+from .base import BaseModel
+from .user import UserModel
+# from .session import SessionModel
+
+
 
 class Instructor(BaseModel):
     __tablename__ = "instructors"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
-    
+    user_id = Column(Integer, ForeignKey('users.id'))
 
-    # Relationships : OneToMany
-    classes = relationship('ClassModel', back_populates='instructors')
+
+    # Relationships : 
+    user = relationship('UserModel', back_populates='teas')
+    # Sessions = relationship('SessionModel', back_populates='instructors')
--- serializers/instructor.py
+++ serializers/instructor.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 
 from serializers.user import UserResponseSchema
-from serializers.class_model import ClassSchema  
+# from serializers.session import SessionSchema  
 
 class InstructorSchema(BaseModel):
     id: Optional[int] = Field(default=None)
     name: str
 
     # Relationships
-    classes: List[ClassSchema] = []  
     user: Optional[UserResponseSchema] = None  
+    # Sessions: List[SessionSchema] = []  
 
     class Config:
         orm_mode = True  
