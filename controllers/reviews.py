01739518f40be03c6877ec5640544c72418e70c3
sara-jaffar <sara.alshoula55@email.com>
2025-08-31 10:14:30 +0300

    created the get one review route

--- controllers/reviews.py
+++ controllers/reviews.py
@@ -33,33 +33,33 @@ def get_session_reviews(category_id: int, session_id: int, db: Session = Depends
     # return reviews for that session
     return session.reviews
 
-# @router.get("/categories/{category_id}/sessions/{session_id}/reviews/{review_id}", response_model=List[ReviewSchema])
-# def get_session_reviews(category_id: int, session_id: int, review_id:int, db: Session = Depends(get_db)):
-#     # check if category exists
-#     category = db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+@router.get("/categories/{category_id}/sessions/{session_id}/reviews/{review_id}", response_model=ReviewSchema)
+def get_session_reviews(category_id: int, session_id: int, review_id:int, db: Session = Depends(get_db)):
+    # check if category exists
+    category = db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
 
-#     if not category:
-#         raise HTTPException(status_code=404, detail="Category not found")
+    if not category:
+        raise HTTPException(status_code=404, detail="Category not found")
 
-#     # check if session exists inside this category
-#     session = (
-#         db.query(SessionModel)
-#         .filter(SessionModel.id == session_id, SessionModel.category_id == category_id)
-#         .first()
-#     )
-#     if not session:
-#         raise HTTPException(status_code=404, detail="Session not found in this category")
+    # check if session exists inside this category
+    session = (
+        db.query(SessionModel)
+        .filter(SessionModel.id == session_id, SessionModel.category_id == category_id)
+        .first()
+    )
+    if not session:
+        raise HTTPException(status_code=404, detail="Session not found in this category")
     
-#     review = db.query(ReviewModel).filter(
-#         ReviewModel.id == review_id,
-#         ReviewModel.session_id == session_id
-#     ).first()
+    review = db.query(ReviewModel).filter(
+        ReviewModel.id == review_id,
+        ReviewModel.session_id == session_id
+    ).first()
 
-#     if not review:
-#         raise HTTPException(status_code=404, detail="Review not found in this session")
+    if not review:
+        raise HTTPException(status_code=404, detail="Review not found in this session")
 
-#     # return reviews for that session
-#     return review
+    # return reviews for that session
+    return review
 
 
 
